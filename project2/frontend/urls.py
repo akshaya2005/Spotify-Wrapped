2729e7e167b8acbc3968417d8c6bda820440e4ad
AAkshaj <akshajevuru@gmail.com>
2024-10-31 18:16:30 -0400

    Do-over

--- project2/frontend/urls.py
+++ project2/frontend/urls.py
@@ -5,6 +5,8 @@ from .views import intro_view
 app_name = 'frontend'
 
 urlpatterns = [
+    path('register', views.register, name="register"),
+    path('login', views.login, name="login"),
     path('', views.index, name=''),
     path('intro/', intro_view, name='intro'),  # Ensure you have a corresponding view
 ]
--- project2/frontend/views.py
+++ project2/frontend/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("search")
+    context = {'loginform': form}
+    return render(request, 'register/my-login.html', context = context)
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST": ##Checking to see whether data must be stored in the database
+        form = CreateUserForm(request.POST) ##Posts values for all the fields to the database
+        if form.is_valid():
+            form.save() ##Saves to database
+
+            return redirect("login")
+
+    context = {'registerform':form}
+
+    return render(request, 'register/register.html', context=context)
+
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
 
