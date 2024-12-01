b0caafd14aeab21e74c24a1fb5aacfeb557b9ead
Akshaya Arun Minnasandran <aminnasandran6@gatech.edu>
2024-12-01 01:02:42 -0500

    Dashboard and settings bug fixes

--- project2/project2/settings.py
+++ project2/project2/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lzv9$dqw@a(@qn28hv&#xxs(&1yyb@evp(ejgjp95xfq_0&a@+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
